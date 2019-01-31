b031db112acd57eedb800d95cf7be81ed52f1591
Marc Fournier <marc.fournier@camptocamp.com>
2019-01-31 16:18:21 +0100

    add initial build script + placeholder Dockerfile

---
+++ travis-ci.sh
@@ -0,0 +1,18 @@
+#!/bin/sh -e
+
+if [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
+  echo "Not building docker images out of Pull Requests"
+  exit 0
+fi
+
+if [ ! -z "${TRAVIS_BRANCH}" ]; then
+  SLUG="collectd/ci:${TRAVIS_BRANCH}"
+  docker built -t "${SLUG}" .
+  docker inspect "${SLUG}"
+  docker history "${SLUG}"
+  echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_LOGIN}" --password-stdin
+  docker push "${SLUG}"
+else
+  echo 'no \$TRAVIS_BRANCH defined, exiting'
+  exit 1
+fi
