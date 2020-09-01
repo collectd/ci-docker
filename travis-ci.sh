52dc987b79ff6986808b6f96a047b7178af7cf0e
Florian Forster <octo@google.com>
2020-09-01 09:49:54 +0200

    travis-ci.sh: Print an error if TRAVIS_BRANCH, DOCKER_PASSWORD, or DOCKER_LOGIN is undefined or empty.

--- travis-ci.sh
+++ travis-ci.sh
@@ -1,18 +1,15 @@
-#!/bin/sh -e
+#!/bin/bash
 
-if [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
+set -ex
+
+if [[ "${TRAVIS_PULL_REQUEST}" != "false" ]]; then
   echo "Not building docker images out of Pull Requests"
   exit 0
 fi
 
-if [ ! -z "${TRAVIS_BRANCH}" ]; then
-  SLUG="collectd/ci:${TRAVIS_BRANCH}"
-  docker build --pull -t "${SLUG}" .
-  docker inspect "${SLUG}"
-  docker history "${SLUG}"
-  echo "${DOCKER_PASSWORD}" | docker login --username "${DOCKER_LOGIN}" --password-stdin
-  docker push "${SLUG}"
-else
-  echo 'no $TRAVIS_BRANCH defined, exiting'
-  exit 1
-fi
+declare -r SLUG="collectd/ci:${TRAVIS_BRANCH:?}"
+docker build --pull -t "${SLUG}" .
+docker inspect "${SLUG}"
+docker history "${SLUG}"
+echo "${DOCKER_PASSWORD:?}" | docker login --username "${DOCKER_LOGIN:?}" --password-stdin
+docker push "${SLUG}"
